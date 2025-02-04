6b4d2589879f5b373535e2c583c948e38fa1840f
zeta114514 <772365121@qq.com>
2025-02-04 14:19:16 +0800

    Add password check program

--- .venv/password_stars.py
+++
@@ -1,8 +0,0 @@
-password = input("Enter password:")
-password_length = 10
-while len(password) < password_length:
-    print("Error")
-    password = input("Enter password:")
-for i in range(len(password)):
-
-    print("*",end = "")
